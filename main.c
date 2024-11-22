f6f5041c4193d49e3aa332de1adef4e24d35573e
Alylaxy <alylaxy.br@gmail.com>
2024-11-22 14:21:08 -0300

    Leitura de caça palavras concluida

--- main.c
+++ main.c
@@ -1,33 +1,43 @@
 // Programinha top criado como nosso próprio filho.
 // Pais: Jão e Lance
 // Propósito: Encontra palavras em um caça palavras
-// Forma de Utilização: Chame o programa com os parâmetros "arquivo", "tamanho da linha", "tamanho da coluna"
+// Forma de Utilização: Chame o programa com os parâmetros "arquivo", "tamanho das colunas", "tamanho das linhas"
 // Arquivo deve ser uma matriz de letras, simulando um caça palavras
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <omp.h>
+#include <omp.h>
 #include <limits.h>
+
+#ifndef max
+    #define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+    #define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
 
 int main(int *argc, char **argv){
     // Definindo variáveis e tratando parâmetros
     FILE *file;
     char *arquivo = argv[1];
-    int tamanho_linha = atoi(argv[2]);
-    int tamanho_coluna = atoi(argv[3]);
+    int n_linhas = atoi(argv[2]);
+    int n_colunas = atoi(argv[3]);
 
-    char linha = (char *)malloc(tamanho_linha * sizeof(char));
+    char *linha = (char *)malloc(n_colunas * sizeof(char));
     file = fopen(arquivo, "r");
 
     int n_palavras;
+    printf("Digite o numero de palavras a serem encontradas: ");
     scanf("%d", &n_palavras);
     char **dicionario = (char **)malloc(n_palavras * sizeof(char *));
 
     // Não é possível que uma palavra tenha mais caracteres que o tamanho da linha ou da coluna
-    int tamanho_palavra = max(tamanho_linha, tamanho_coluna);
+    int tamanho_palavra = max(n_linhas, n_colunas);
     for(int i = 0; i < n_palavras; i++){
         dicionario[i] = (char *)malloc(tamanho_palavra * sizeof(char));
+        printf("Digite a palavra %d: ", i+1);
         scanf("%s", dicionario[i]);
     }
 
@@ -38,11 +48,20 @@ int main(int *argc, char **argv){
     }
 
     // Lê cada linha do arquivo e salva em linha. Preferi fazer com um for e numero de colunas para facilitar a paralelização.
-    for(int i = 0; i < tamanho_coluna; i++){
-        fgets(linha, tamanho_linha, file);
-        printf("%s", linha);
+    for(int i = 0; i < n_linhas; i++){
+        if (fgets(linha, n_colunas + 2, file) != NULL) {
+            printf("%s", linha);
+        } else {
+            break; // Sai do loop se não conseguir ler mais linhas
+        }
     }
 
+    // Free em tudo que foi alocado
+    for(int i = 0; i < n_palavras; i++){
+        free(dicionario[i]);
+    }
+    free(dicionario);
+    free(linha);
     fclose(file);
     return 0;
 }
